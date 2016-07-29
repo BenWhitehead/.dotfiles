3be5d29e3dae40e30ed5d70d5ac33cd31a90daa9
BenWhitehead <ben.whitehead@mesosphere.io>
2016-07-29 14:35:45 -0700

    Add csv utility functions

---
+++ fs/home/user/.bash/function/csv.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+function csvToTsv {
+  awk '{$1=$1}1' FPAT="([^,]+)|(\"[^\"]+\")" OFS='\t' $1
+}
+
+function readcsv {
+  column -t -s ',' ${1:--} | less -S -# 10
+}
+
+function readtsv {
+  column -t -s $'\t' ${1:--} | less -S -# 10
+}
+
