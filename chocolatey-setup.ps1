227e41aaec5101d29711fc819676b2746cc6ee7e
BenWhitehead <BenWhitehead@users.noreply.github.com>
2021-08-29 09:14:30 -0400

    Add chocolaty-setup.ps1

---
+++ chocolatey-setup.ps1
@@ -0,0 +1,5 @@
+# https://docs.chocolatey.org/en-us/choco/commands/install
+choco install git -y --install-arguments="'/DIR=C:\opt\Git'" --params="'/NoShellIntegration /NoGuiHereIntegration /NoShellHereIntegration /NoCredentialManager'"
+choco install hackfont- -y
+choco install krita -y
+choco install vlc -y
