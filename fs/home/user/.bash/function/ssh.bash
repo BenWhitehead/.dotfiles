cde68c1f43f6b8f110e9566e26be252346556779
BenWhitehead <BenWhitehead@users.noreply.github.com>
2022-03-07 12:41:56 -0500

    Remove agent forwarding for sshtmp and scptmp

--- fs/home/user/.bash/function/ssh.bash
+++ fs/home/user/.bash/function/ssh.bash
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 function sshtmp {
-	ssh -o "IdentitiesOnly yes" -o "ForwardAgent yes" -o "UserKnownHostsFile /dev/null" -o "StrictHostKeyChecking no" $@
+	ssh -o "IdentitiesOnly yes" -o "UserKnownHostsFile /dev/null" -o "StrictHostKeyChecking no" $@
 }
 
 function scptmp {
-	scp -o "IdentitiesOnly yes" -o "ForwardAgent yes" -o "UserKnownHostsFile /dev/null" -o "StrictHostKeyChecking no" $@
+	scp -o "IdentitiesOnly yes" -o "UserKnownHostsFile /dev/null" -o "StrictHostKeyChecking no" $@
 }
 
