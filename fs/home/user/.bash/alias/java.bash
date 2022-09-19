cd2aaf65925f96d49292c1185c0ea28e93147cd5
BenWhitehead <BenWhitehead@users.noreply.github.com>
2022-09-19 16:03:43 -0400

    add quick aliases to activate various java versions

--- fs/home/user/.bash/alias/java.bash
+++ fs/home/user/.bash/alias/java.bash
@@ -3,3 +3,22 @@
 #alias unzip='jar xf'
 
 alias java8='JAVA_HOME=$JAVA_8_HOME $JAVA_8_HOME/bin/java'
+
+# Inspired by https://stackoverflow.com/a/40754792
+function __activateJavaHome() {
+  local dir=$1
+  if [ -d "$dir" ]; then
+    export JAVA_HOME="$dir" && \
+    echo "JAVA_HOME=$JAVA_HOME" && \
+    $JAVA_HOME/bin/java -version
+  else
+    echo "Specified JAVA_HOME directory '$dir' is not a directory."
+    return 1
+  fi
+}
+
+alias  j8='__activateJavaHome $JAVA_8_HOME'
+alias j11='__activateJavaHome $JAVA_11_HOME'
+alias j17='__activateJavaHome $JAVA_17_HOME'
+alias j19='__activateJavaHome $JAVA_19_HOME'
+
