d9e0ff7fce3f0309f0484ab95bda6b1746504449
BenWhitehead <BenWhitehead@users.noreply.github.com>
2025-12-29 15:18:34 -0500

    java cleanup

--- fs/home/user/.bash/alias/java.bash
+++ fs/home/user/.bash/alias/java.bash
@@ -20,9 +20,6 @@ function __activateJavaHome() {
 alias  j8='__activateJavaHome $JAVA_8_HOME'
 alias j11='__activateJavaHome $JAVA_11_HOME'
 alias j17='__activateJavaHome $JAVA_17_HOME'
-alias j19='__activateJavaHome $JAVA_19_HOME'
-alias j20='__activateJavaHome $JAVA_20_HOME'
 alias j21='__activateJavaHome $JAVA_21_HOME'
-alias j22='__activateJavaHome $JAVA_22_HOME'
-alias j23='__activateJavaHome $JAVA_23_HOME'
+alias j25='__activateJavaHome $JAVA_25_HOME'
 
--- fs/home/user/.bash/env/java.bash
+++ fs/home/user/.bash/env/java.bash
@@ -1,23 +1,11 @@
 #!/bin/bash
 
-export JAVA_23_HOME=~/opt/java/23
-export JAVA_22_HOME=~/opt/java/22
+export JAVA_25_HOME=~/opt/java/25
 export JAVA_21_HOME=~/opt/java/21
-export JAVA_20_HOME=~/opt/java/20
-export JAVA_19_HOME=~/opt/java/19
 export JAVA_17_HOME=~/opt/java/17
-export JAVA_16_HOME=~/opt/java/16
-export JAVA_15_HOME=~/opt/java/15
-export JAVA_14_HOME=~/opt/java/14
-export JAVA_13_HOME=~/opt/java/13
-export JAVA_12_HOME=~/opt/java/12
 export JAVA_11_HOME=~/opt/java/11
-export JAVA_9_HOME=~/opt/java/1.9
 export JAVA_8_HOME=~/opt/java/1.8
-export JAVA_7_HOME=~/opt/java/1.7
 
-#export IDEA_JDK=$JAVA_8_HOME
-
-export JAVA_HOME=$JAVA_8_HOME
+export JAVA_HOME=~/opt/java/latest
 export PATH=${JAVA_HOME}/bin:${PATH}
 
